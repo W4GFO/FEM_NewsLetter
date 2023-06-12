5e5640b7ab3e4586e4cbc36ff0c758599208d1a8
Jeff Behrbaum <sejapresident@gmail.com>
2023-06-12 16:35:09 -0400

    More-less finished laying out the desktop version of the page.

--- news-letter/src/Components/MarketingMaterialFrame.tsx
+++ news-letter/src/Components/MarketingMaterialFrame.tsx
@@ -7,11 +7,17 @@ export const MarketingMaterialFrame = (props: MarketingMaterialProps) => {
 	return (
 		<div className='marketing_material_text'>
 			<h1>Stay Updated!</h1>
-			<p>Join 60,000+ product managers receiving monthly updates on:</p>
+			<p className='marketing_message'>Join 60,000+ product managers receiving monthly updates on:</p>
 
 			<CheckElement displayText="Product discovery and building what matters"/>
 			<CheckElement displayText="Measuring to ensure updates are a success"/>
 			<CheckElement displayText="And much more!"/>
+
+			<form>
+				<label htmlFor="email">Email Address:</label>
+				<input type="email" placeholder="example@company.com" />
+				<button type="submit">Subscribe to monthly newsletter</button>
+			</form>
 		</div>
 	)
 }
